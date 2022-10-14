6e0355404548f1d8c0b6c9ef133c8f7eef7942cb
Bharti Meena <60185429+bhartimeena@users.noreply.github.com>
2022-10-14 10:47:33 +0530

    hacktoberFest2022

---
+++ C++/half_pyramid.cpp
@@ -0,0 +1,18 @@
+// APNI KAKSHA DSA SERIES
+// Print Half Pyramid Pattern using Stars
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return(0);
+}
